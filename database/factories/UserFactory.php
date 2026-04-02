bd3968e6327af2469654c4e287e0fad62ebd0fe8
panca <ykzaditia5@gmail.com>
2026-04-02 14:01:16 +0700

    wip

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use Faker\Factory as FakerFactory;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -23,10 +24,12 @@ class UserFactory extends Factory
      */
     public function definition(): array
     {
+        $faker = FakerFactory::create();
+        
         return [
-            'username' => $this->faker->userName(),
-            'nis' => $this->faker->numerify('############'),
-            'email' => $this->faker->unique()->safeEmail(),
+            'username' => $faker->userName(),
+            'nis' => $faker->numerify('############'),
+            'email' => $faker->unique()->safeEmail(),
             'role' => 'siswa',
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
